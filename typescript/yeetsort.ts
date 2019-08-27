c158027d989159ab84277e2577f14799ca68de56
Zack Littke-Smith <zack.littkesmith@gmail.com>
2019-08-26 20:45:04 -0700

    add typescript

---
+++ typescript/yeetsort.ts
@@ -0,0 +1,11 @@
+
+const yeetsort = (a: any[]) => {
+	// handle edge case
+	if (a.length === 0) {
+		throw Error("This bitch empty, YEET!");
+	}
+
+	// regular yeet
+	throw Error("Yeet");
+}
+export default yeetsort;
