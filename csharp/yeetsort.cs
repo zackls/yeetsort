06e20ed9e6336fa935d5313cd5b20475d620bca6
Zack Littke-Smith <zack.littkesmith@gmail.com>
2019-08-26 20:52:32 -0700

    add c#

---
+++ csharp/yeetsort.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+public void YeetSort<T>(this List<T> a)
+{
+	// handle edge case
+	if (!a.Any()) {
+		throw new System.Exception("This bitch empty, YEET!");
+	}
+
+	// regular yeet
+	throw new System.Exception("Yeet");
+}
